b16ec745b220a6ed64a48e824386fef0841160a7
Mayank <mayankchaurasiya66@gmail.com>
2025-06-11 22:50:30 +0530

    updated simple calculator

--- Projects/Simple_Calculator.c
+++ Projects/Simple_Calculator.c
@@ -1,1 +1,16 @@
 // A simple calculator is a program that can perform addition, subtraction, multiplication, and division of two numbers provided as input.
+# include <stdio.h>
+# include <float.h>
+
+int main() {
+    char op;
+    double a, b, result;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &op);
+
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &a, &b);
+
+    
+}
