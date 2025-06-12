ff5d0a0522e7027c6559879f64dfbdeae584ceb6
Mayank <mayankchaurasiya66@gmail.com>
2025-06-12 14:22:31 +0530

    updated BattleShip game

--- Projects/GameOfBattleShip.c
+++ Projects/GameOfBattleShip.c
@@ -20,5 +20,27 @@ int main()
         scanf("%d", &row);
 
         printf("Choose a column number : ");
+        scanf("%d", &column);
+
+        // check if a ship exists in those coordinates
+        if (ships[row][column])
+        {
+            // if the player hit a ship, remove it by setting the value to zero.
+            ships[row][column] = 0;
+
+            // Increase the hit counter
+            hits++;
+
+            // Tell the player that they have hit a ship and how many ships are left.
+            printf("Hit! %d left.\n\n", 4 - hits);
+        }
+        else
+        {
+            // Tell the player that they missed
+            printf("Miss!");
+        }
+        numberOfTurns++;
     }
+    printf("Victory!\n");
+    printf("You won in %d turns", numberOfTurns);
 }
