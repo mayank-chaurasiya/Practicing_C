29e82f582a70364dedfcde9fa778b23b9c8f8ba5
Mayank <mayankchaurasiya66@gmail.com>
2025-06-30 14:17:40 +0530

    updated hungman game

--- Projects/Hangman_Game.c
+++ Projects/Hangman_Game.c
@@ -4,3 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+
+#define MAX_WORD_LENGTH 50
+#define MAX_TRIES 6
+
+struct WordWithHint
+{
+    char word[MAX_WORD_LENGTH];
+    char hint[MAX_WORD_LENGTH];
+};
+
+// Function to display current state of the word
+void displayWord(const char word[], const bool guessed[]);
