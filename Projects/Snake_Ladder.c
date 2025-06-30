c0deeb2c5aeff3d181f0dac55c01797b0da7a897
Mayank <mayankchaurasiya66@gmail.com>
2025-06-30 16:27:46 +0530

    updated Snake Ladder.c

--- Projects/Snake_Ladder.c
+++ Projects/Snake_Ladder.c
@@ -79,4 +79,24 @@ void printBoard()
     printf("\n");
 }
 
+// Function to move the player
+int movePlayer(int currentPlayer, int roll)
+{
+    int newPosition = currentPlayer + roll;
+    // define the positions of snakes and ladderes on the board
+    int snakesAndLadders[101];
 
+    for (int i = 0; i <= 100; i++)
+    {
+        snakesAndLadders[i] = 0;
+    }
+
+    // here positive weights represent a ladder and negative weights represent a snake.
+    snakesAndLadders[6] = 40;
+    snakesAndLadders[23] = -10;
+    snakesAndLadders[45] = -7;
+    snakesAndLadders[61] = -18;
+    snakesAndLadders[65] = -8;
+    snakesAndLadders[77] = 5;
+    snakesAndLadders[98] = -10;
+}
