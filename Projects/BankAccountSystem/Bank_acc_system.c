4ed793172e0e67f0e76620004b9aa2d3d9998c07
Mayank <mayankchaurasiya66@gmail.com>
2025-07-01 23:01:34 +0530

    added checkbalance function

--- Projects/BankAccountSystem/Bank_acc_system.c
+++ Projects/BankAccountSystem/Bank_acc_system.c
@@ -482,3 +482,66 @@ void transfermoney(void)
     // function to return to the home screen
     display(usernamet);
 }
+
+// Function to check balance in users account
+void checkbalance(char username2[])
+{
+    system("cls");
+    FILE *fm;
+    struct money m1;
+    char ch;
+    int i = 1, summoney = 0;
+
+    // opening amount file record
+    fm = fopen("mon.txt", "rb");
+
+    int k = 5, l = 10, m = 30, n = 10, u = 60, v = 10;
+
+    gotoxy(30, 2);
+    printf("==== BALANCE DASHBOARD ====");
+
+    gotoxy(30, 3);
+    printf("***************************");
+
+    gotoxy(k, l);
+    printf("S no.");
+
+    gotoxy(m, n);
+    printf("TRANSACTION ID");
+
+    gotoxy(u, v);
+    printf("AMOUNT");
+
+    // Reading username to fetch the correct record
+    while (fread(&m1, sizeof(m1), 1, fm))
+    {
+        if (strcmp(username2, m1.usernameto) == 0)
+        {
+            gotoxy(k, ++l);
+            printf("%d", i);
+
+            i++;
+
+            gotoxy(m, ++n);
+            printf("%s", m1.userpersonfrom);
+
+            gotoxy(u, ++v);
+            printf("%d", m1.money1);
+
+            // Adding and finding total money
+            summoney = summoney + m1.money1;
+        }
+    }
+
+    gotoxy(80, 10);
+    printf("TOTOAL AMOUNT");
+
+    gotoxy(80, 12);
+    printf("%d", summoney);
+
+    getch();
+
+    // Closing the file after readint it
+    fclose(fm);
+    display(username2);
+}
