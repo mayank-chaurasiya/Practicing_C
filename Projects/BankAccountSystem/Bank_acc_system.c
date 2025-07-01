9ecf8421a57b86be616eab7f7b54571a2cefafa9
Mayank <mayankchaurasiya66@gmail.com>
2025-07-01 20:29:24 +0530

    updated bank account system

--- Projects/BankAccountSystem/Bank_acc_system.c
+++ Projects/BankAccountSystem/Bank_acc_system.c
@@ -67,5 +67,19 @@ int main()
 
     printf("**********************************");
     gotoxy(25, 7);
-    
+
+    printf("DEVELOPER-Mayank");
+
+    gotoxy(20, 10);
+    printf("1.... CREATE A BANK ACCOUNT");
+
+    gotoxy(20, 12);
+    printf("2.... ALREADY A USER? SIGN IN");
+
+    gotoxy(20, 14);
+    printf("3.... EXITn\n\n");
+
+    printf("\n\nENTER YOUR CHOICE..");
+
+    scanf("%d", &choice);
 }
