b7fcd33bb8a9c49239cc25df968fd33f03663c74
Mayank <mayankchaurasiya66@gmail.com>
2025-07-01 21:34:53 +0530

    added accountcreated function

--- Projects/BankAccountSystem/Bank_acc_system.c
+++ Projects/BankAccountSystem/Bank_acc_system.c
@@ -186,3 +186,27 @@ void account(void)
     // Calling another function after successful creation of account
     accountcreated();
 }
+
+// Successful account creation
+void accountcreated(void)
+{
+    int i;
+    char ch;
+    system("cls");
+    printf("PLEASE WAIT....\n\nYOUR DATA IS PROCESSING....");
+    for (i = 0; i < 20000000; i++)
+    {
+        i++;
+        i--;
+    }
+
+    gotoxy(30, 10);
+
+    printf("ACCOUNT CREATED SUCCESSFULLY....");
+    gotoxy(0, 20);
+
+    printf("Press enter to login");
+
+    getch();
+    login();
+}
