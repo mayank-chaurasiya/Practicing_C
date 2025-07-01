cede5e562f40678fce330d3393cffe6cd7ea090c
Mayank <mayankchaurasiya66@gmail.com>
2025-07-01 23:04:11 +0530

    added Logout function

--- Projects/BankAccountSystem/Bank_acc_system.c
+++ Projects/BankAccountSystem/Bank_acc_system.c
@@ -545,3 +545,29 @@ void checkbalance(char username2[])
     fclose(fm);
     display(username2);
 }
+
+// Logout function to bring user tho the login screen
+void logout(void)
+{
+    int i, j;
+    system("cls");
+    printf("please wait, loggint out");
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 25000000; j++)
+        {
+            i++;
+            i--;
+        }
+        printf(".");
+    }
+
+    gotoxy(30, 10);
+    printf("Sign out successfully.. \n");
+
+    gotoxy(0, 20);
+    printf("press any key to continue..");
+
+    getch();
+}
