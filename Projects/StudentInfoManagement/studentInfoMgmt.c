67b02fdbacc89212e0f72bdcb91805e6e381ea37
Mayank <mayankchaurasiya66@gmail.com>
2025-07-04 23:45:02 +0530

    updated with findbyRoll() function

--- Projects/StudentInfoManagement/studentInfoMgmt.c
+++ Projects/StudentInfoManagement/studentInfoMgmt.c
@@ -40,3 +40,28 @@ void addStudent()
     }
     i++;
 }
+
+// function to find the student by the roll number
+void findByRoll()
+{
+    int x;
+    printf("Enter the roll number of the individual \n");
+    scanf("%d", &x);
+
+    for (int j = 1; j <= i; j++)
+    {
+        if (x == student[i].roll)
+        {
+            printf("The Students Details are :- \n");
+            printf("Full name : %s %s\n", student[i].firstName, student[i].lastName);
+            printf("Roll no. : %d\n", student[i].roll);
+            printf("CGPA scored : %f \n", student[i].cgpa);
+            printf("Course id's : ");
+        }
+        for (int j = 0; j < 5; j++)
+        {
+            printf("The Course ID are %d\n", student[i].courseId[j]);
+        }
+        break;
+    }
+}
