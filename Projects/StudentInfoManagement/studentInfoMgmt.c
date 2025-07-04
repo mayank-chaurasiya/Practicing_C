31e10f26f435c5105d2275a2b202918f171514b5
Mayank <mayankchaurasiya66@gmail.com>
2025-07-04 23:15:00 +0530

    Adding Student Information Management System

---
+++ Projects/StudentInfoManagement/studentInfoMgmt.c
@@ -0,0 +1,9 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// variable to keep track of number of students
+int i = 0;
+
+// Structure to store the student information 
